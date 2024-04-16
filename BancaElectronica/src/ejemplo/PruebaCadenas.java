c0d2c1b4d492211631161f638f33d45997fb2278
Johan Otalvaro <jfotalva@bancolombia.com.co>
2024-04-16 15:29:24 -0500

    Notas de clase

--- BancaElectronica/src/ejemplo/PruebaCadenas.java
+++ BancaElectronica/src/ejemplo/PruebaCadenas.java
@@ -15,10 +15,20 @@ public class PruebaCadenas {
         OperaCadenas concatena = (c1, c2) -> c1 + c2;
         System.out.println(concatena.opera("!!Hola ", "Mundo¡¡"));
 
-        //OperaCadenas iniciaCon =
-        //OperaCadenas terminaCon =
-        //OperaCadenas comparaIgnorandoMayMin =
-        //OperaCadenas buscaCadena = La cadena "es" esta en "Hoy es dia jueves" "Si/No"
+        OperaCadenas iniciaCon = (c1, c2) -> c1.startsWith(c2) ? "La cadena inicia con " + c2 : "La cadena no inicia con " + c2;
+        System.out.println(iniciaCon.opera("Programando en Java", "Programando"));
+
+        OperaCadenas terminaCon = (c1, c2) -> c1.endsWith(c2) ? "La cadena termina con " + c2
+                : "La cadena no termina con " + c2;
+        System.out.println(terminaCon.opera("Hola Mundo", "Mundo"));
+
+        OperaCadenas comparaIgnorandoMayMin = (c1, c2) -> c1.equalsIgnoreCase(c2)
+                ? "Cadenas iguales ignorando mayúsculas y minusculas"
+                : "Cadenas diferentes ignorando mayúsculas y minúsculas";
+        System.out.println(comparaIgnorandoMayMin.opera("Esta CaDENa Esta MAL", "esta cadena esta mal"));
+
+        OperaCadenas buscaCadena = (c1, c2) -> c1.contains(c2) ? "La cadena contiene " + c2 : "La cadena no contiene " + c2;
+        System.out.println(buscaCadena.opera("es", "Hoy es dia jueves"));
     }
 }
 
